156196af7049a79d56cee079093a88995c57c0c0
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-02-09 17:00:00 +0100

    create a function that adds a constant card
    this card is constant meaning that the name and the picture
    are "hard-coded" in the function.

--- script.js
+++ script.js
@@ -24,3 +24,12 @@ const animalsToAdopt = [
 // Deck container
 const nodeDeck = document.querySelector(".deck");
 
+function addCard() {
+    const cardHTML = `
+    <div class="card">
+        <h4>Lucky</h4>
+        <img src="https://placekitten.com/200/287" alt="">
+    </div>`;
+
+    nodeDeck.innerHTML += cardHTML;
+}
